58aa069da1d00288e2a73f8fa0f89e93af2275cf
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-01-12 10:59:51 +0100

    Nueva rama bugFix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Rama pricipal");
+        System.out.println("Salgo de la rama pricipal");
+        System.out.println("Voy a la bugFix");
     }
 }
