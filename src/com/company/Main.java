a5db6b2df3877ec37723711ffe2014491d6dff51
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-01-12 11:06:48 +0100

    Modificaciones en la principal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Rama pricipal");
+        System.out.println("Hago modificaciones en la rama principal");
     }
 }
