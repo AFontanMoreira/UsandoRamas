3158304769e49c6e62e74a222dbbac33fe12fe91
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-01-12 11:29:22 +0100

    Desplazo lineas para evitar conflicto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+
         System.out.println("Salgo de la rama pricipal");
         System.out.println("Voy a la bugFix");
     }
